38a5b34158e4d1a7ba456019d14ce5222f5ef831
firefox35 <f1firefox@yahoo.ie>
2023-11-02 06:27:12 +0000

    remove key

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get(
-    'SECRET_KEY', 'django-insecure-*9q4-i!un)8$fzgsk1z^b@k7339d_kf_fdr+zc)j!-%26ki1ju')
+    'SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = development
